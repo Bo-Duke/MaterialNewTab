11ce1a6a5ae14511a173847dc25a9940dc06fa1f
Bo-Duke <maxime.bouveron@gmail.com>
2015-05-26 20:17:53 +0200

    Links are now loaded from JSON

--- script.js
+++ script.js
@@ -16,3 +16,30 @@ else if (hour >= 20){
 else if (hour >= 0 && hour < 5){
 	changeBg("night");
 }
+
+var data;
+request = new XMLHttpRequest();
+request.open('GET', 'links.json', true);
+
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400){
+    data = JSON.parse(request.responseText);
+
+    for (var i = 0; i < data.links.length; i++) {
+    	var activeLink = document.getElementById("link_"+(i+1));
+    	activeLink.href=data.links[i].url;
+    	activeLink.firstElementChild.innerHTML = data.links[i].name;
+    	activeLink.style.backgroundImage = "url('"+data.links[i].icon+"')";
+    	activeLink.style.backgroundColor = data.links[i].color;
+    }
+  } else {
+    
+
+  }
+};
+
+request.onerror = function() {
+  
+};
+
+request.send();
