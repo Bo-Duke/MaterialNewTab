0bce0d635cd9cf1fcf18a25ae1d2976e295e8f7e
Bo-Duke <maxime.bouveron@gmail.com>
2015-05-31 20:56:51 +0200

    The options page is here but is still useless

---
+++ optionsScript.js
@@ -0,0 +1,57 @@
+function changeBg(name){
+	document.body.style.backgroundImage = "url('img/mountains"+name+".jpg')";
+}
+
+var hour = new Date().getHours();
+
+if(hour >= 5 && hour < 9){
+	changeBg("dawn");
+}
+else if (hour >= 9 && hour < 20){
+	changeBg("day");
+}
+else if (hour >= 20){
+	changeBg("dusk");
+}
+else if (hour >= 0 && hour < 5){
+	changeBg("night");
+}
+
+var data;
+request = new XMLHttpRequest();
+request.open('GET', 'links.json', true);
+
+request.onload = function() {
+	if (request.status >= 200 && request.status < 400){
+		data = JSON.parse(request.responseText);
+
+		for (var i = 0; i < data.links.length; i++) {
+			var activeLink = document.getElementById("link_"+(i+1));
+			activeLink.href=data.links[i].url;
+			activeLink.firstElementChild.innerHTML = data.links[i].name;
+			activeLink.style.backgroundImage = "url('"+data.links[i].icon+"')";
+			activeLink.style.backgroundColor = data.links[i].color;
+
+			var activeForm = document.forms["form_"+(i+1)];
+			activeForm["Name"].value = data.links[i].name;
+			activeForm["url"].value = data.links[i].url;
+			activeForm["icon"].value = data.links[i].icon;
+			activeForm["color"].value = data.links[i].color;
+		}
+	} else {
+		
+
+	}
+};
+
+request.onerror = function() {
+	
+};
+
+request.send();
+
+for (var i = 0; i<document.forms.length; i++){
+	console.log(document.forms["form_"+(i+1)]["Name"]);
+	// var obj = { name: 'Dhayalan', score: 100 };
+	// localStorage.setItem('gameStorage', JSON.stringify(obj));
+}
